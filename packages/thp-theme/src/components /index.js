a435b213b88999672dce88142c93ae5553cc60e0
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-02-11 20:53:32 +0000

    links added

--- packages/thp-theme/src/components /index.js
+++ packages/thp-theme/src/components /index.js
@@ -1,6 +1,7 @@
 import { connect, Global, Head, styled, css } from "frontity";
 import React from "react";
 import Link from "./Link";
+import Link from "@frontity/components/link";
 import { globalStyles } from "./style/global-style";
 
 const Root = ({ state }) => {
