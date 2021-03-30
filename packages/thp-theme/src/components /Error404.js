cb5c1716b744c2cac8b16814b8fd98906fc15698
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-03-30 06:33:19 +0100

    404 page

--- packages/thp-theme/src/components /Error404.js
+++ packages/thp-theme/src/components /Error404.js
@@ -1,4 +1,44 @@
-const Error404 = () => {
-  return <div>Eror</div>;
+import React from "react";
+import { connect, styled } from "frontity";
+import { Primary } from "./reusableComponents/buttons";
+import { ContainerDiv } from "./reusableComponents/container";
+const Error404 = ({ state }) => {
+  const breakpoints = state.theme.breakpoints;
+  return (
+    <ContainerDiv>
+      <Container404 breakpoints={breakpoints}>
+        <CredentialsContainer>
+          <h1>It looks like you are lost</h1>
+          <p>The page you are looking is not available</p>
+          <Primary>Go Home</Primary>
+        </CredentialsContainer>
+        <Text404>404</Text404>
+      </Container404>
+    </ContainerDiv>
+  );
 };
-export default Error404;
+export default connect(Error404);
+
+const CredentialsContainer = styled.div`
+  h1 {
+    font-weight: 300;
+  }
+`;
+
+const Text404 = styled.p`
+  font-size: 20rem;
+  font-weight: 500;
+  color: var(--secondary-colour);
+`;
+
+const Container404 = styled.div`
+  padding: 0 2em;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+  @media screen and (min-width: ${(props) => props.breakpoints.tablet}) {
+    flex-direction: row;
+  }
+`;
