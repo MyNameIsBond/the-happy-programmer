da7bb3f19fd88b796ee0be4ebd273acdca3cdfd8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-04-23 06:28:30 +0100

    patreon Button Added

---
+++ build/static/the-happy-programmer.ba48e3601e3458ca5317.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("the-happy-programmer",{
+
+/***/ "./packages/thp-theme/src/components /Archive/Category.js":
+/*!****************************************************************!*\
+  !*** ./packages/thp-theme/src/components /Archive/Category.js ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/tony/Documents/the-happy-programmer/packages/thp-theme/src/components /Archive/Category.js: Unexpected token (32:6)\\n\\n\\u001b[0m \\u001b[90m 30 |\\u001b[39m \\u001b[36mexport\\u001b[39m \\u001b[36mdefault\\u001b[39m connect(\\u001b[33mCategory\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 31 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 32 |\\u001b[39m \\u001b[36mconst\\u001b[39m \\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m       \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n    at Object._raise (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:97:45)\\n    at Object.raiseWithData (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:92:17)\\n    at Object.raise (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:41:17)\\n    at Object.unexpected (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/util.js:161:16)\\n    at Object.parseIdentifierName (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:2318:18)\\n    at Object.parseIdentifier (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:2284:23)\\n    at Object.parseBindingAtom (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/lval.js:299:17)\\n    at Object.parseVarId (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/statement.js:1050:20)\\n    at Object.parseVar (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/statement.js:1012:12)\\n    at Object.parseVarStatement (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/statement.js:706:10)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL3RocC10aGVtZS9zcmMvY29tcG9uZW50cyAvQXJjaGl2ZS9DYXRlZ29yeS5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./packages/thp-theme/src/components /Archive/Category.js\n");
+
+/***/ }),
+
+/***/ "./packages/thp-theme/src/components /Archive/CategoryStyle.js":
+false
+
+})
--- packages/thp-theme/src/components /Archive/Category.js
+++ packages/thp-theme/src/components /Archive/Category.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Primary } from "../reusableComponents/buttons";
-import { connect } from "frontity";
+import { connect, styled } from "frontity";
 import { CategoryContainer } from "./CategoryStyle";
 import Link from "@frontity/components/link";
 import { getData } from "../Handlers/dataManager";
-
+import { default as patreonIcon } from "../constants/patreonIcon.svg";
 const Category = ({ category, state }) => {
   const data = getData(state);
   const { isPostType, isPost } = data;
@@ -18,13 +18,41 @@ const Category = ({ category, state }) => {
           </Primary>
         </Link>
       ))}
-      {(isPost && isPostType) ?? (
-        <Primary>
-          <span>be a Patreon</span>
-        </Primary>
+      {isPostType && (
+        <Patreonbutton>
+          <Link link="https://www.patreon.com/thehappyprogrammer">
+            <Primary>
+              <PatreonImg src={patreonIcon} />
+              <span>Become a Patreon</span>
+            </Primary>
+          </Link>
+        </Patreonbutton>
       )}
     </CategoryContainer>
   );
 };
 
 export default connect(Category);
+
+const PatreonImg = styled.img`
+  height: 0.9em;
+  width: auto;
+  filter: brightness(0) invert(1);
+  padding-right: 0.3em;
+`;
+
+const Patreonbutton = styled.div`
+  button {
+    text-transform: capitalize;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background: #eb5354;
+    color: white;
+    border-color: #eb5354;
+  }
+  button:hover {
+    border-color: #eb5354;
+    background: #eb5354;
+  }
+`;
