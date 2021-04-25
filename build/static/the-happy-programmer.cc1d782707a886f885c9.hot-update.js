40599af74f239cdb58040d9a1231cc0bda6110b8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-04-25 21:00:26 +0100

    cc

---
+++ build/static/the-happy-programmer.cc1d782707a886f885c9.hot-update.js
@@ -0,0 +1,56 @@
+webpackHotUpdate("the-happy-programmer",{
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Form.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/HiddenInput.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Input.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Message.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Select.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Span.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/components/Textarea.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/context/FormIdContext.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/index.js":
+/*!***********************************************************!*\
+  !*** ./node_modules/frontity-contact-form-7/src/index.js ***!
+  \***********************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/tony/Documents/the-happy-programmer/node_modules/frontity-contact-form-7/src/index.js: Unexpected token, expected \\\",\\\" (100:30)\\n\\n\\u001b[0m \\u001b[90m  98 |\\u001b[39m \\t\\t\\t\\t\\u001b[36mconst\\u001b[39m body \\u001b[33m=\\u001b[39m \\u001b[36mawait\\u001b[39m res\\u001b[33m.\\u001b[39mjson()\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  99 |\\u001b[39m \\t\\t\\t\\tconsole\\u001b[33m.\\u001b[39mlog(\\u001b[32m\\\"POST:\\\"\\u001b[39m\\u001b[33m,\\u001b[39mbody)\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 100 |\\u001b[39m \\t\\t\\t\\t\\u001b[36mlet\\u001b[39m invalidFieldsObj \\u001b[33m=\\u001b[39m []}\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m     |\\u001b[39m \\t\\t\\t\\t                          \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 101 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 102 |\\u001b[39m \\t\\t\\t\\t\\u001b[90m// Clear previous message.\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 103 |\\u001b[39m \\t\\t\\t\\tstate\\u001b[33m.\\u001b[39mcf7\\u001b[33m.\\u001b[39mforms[ id ]\\u001b[33m.\\u001b[39mmessage \\u001b[33m=\\u001b[39m []}\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Object._raise (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:97:45)\\n    at Object.raiseWithData (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:92:17)\\n    at Object.raise (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/error.js:41:17)\\n    at Object.unexpected (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/util.js:161:16)\\n    at Object.expect (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/util.js:138:28)\\n    at Object.parseObjectLike (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:1637:14)\\n    at Object.parseExprAtom (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:1110:21)\\n    at Object.parseExprAtom (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/plugins/jsx/index.js:567:22)\\n    at Object.parseExprSubscripts (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:611:23)\\n    at Object.parseUpdate (/Users/tony/Documents/the-happy-programmer/node_modules/@babel/parser/src/parser/expression.js:591:21)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL25vZGVfbW9kdWxlcy9mcm9udGl0eS1jb250YWN0LWZvcm0tNy9zcmMvaW5kZXguanMuanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./node_modules/frontity-contact-form-7/src/index.js\n");
+
+/***/ }),
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7Form.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7HiddenInputs.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7Inputs.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7Select.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7Span.js":
+false,
+
+/***/ "./node_modules/frontity-contact-form-7/src/processors/cf7Textarea.js":
+false
+
+})
--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   name: "the-happy-programmer",
   state: {
     frontity: {
-      url: "https://thehappyprogrammer.com/",
+      url: "https://thehappyprogrammer.com",
       title: "The Happy Programmer",
       description: "Mobile and web free courses",
     },
