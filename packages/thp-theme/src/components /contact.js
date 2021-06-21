b7976e4fe9fb2c75db8760f534738b07d671f1fa
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-06-21 22:35:00 +0100

    cc

--- packages/thp-theme/src/components /contact.js
+++ packages/thp-theme/src/components /contact.js
@@ -2,6 +2,6 @@ import { styled } from "frontity";
 
 export const Contact = () => <Background>Contact me</Background>;
 const Background = styled.div`
-  background: yellowgreen;
+  background: yellow;
   height: 100%;
 `;
